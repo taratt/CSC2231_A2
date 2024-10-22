760edefa6163cd2621a5f0247dc7e5553b5ed131
taratt <taratt.saba@gmail.com>
2024-10-22 02:44:55 -0400

    part 2

--- part2-src/attacker-part2.c
+++ part2-src/attacker-part2.c
@@ -51,7 +51,7 @@ int run_attacker(char *shared_memory) {
         bool accessed = 0;
         for (int iteration = 0; iteration < num_iterations; iteration++) {
             init_shared_memory(shared_memory, SHD_SPECTRE_LAB_SHARED_MEMORY_SIZE);
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 10000; i++) {
                 call_kernel_part2(shared_memory, 0);
             }
 
