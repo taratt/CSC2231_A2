1e33bca5e5a371b414a3d9493ae88fdfcfd8e5aa
taratt <taratt.saba@gmail.com>
2024-10-22 04:37:52 -0400

    part 2

--- part2-src/attacker-part2.c
+++ part2-src/attacker-part2.c
@@ -50,7 +50,7 @@ int run_attacker(char *shared_memory) {
         // leaked_byte = ??
         for (int iteration = 0; iteration < num_iterations; iteration++) {
             init_shared_memory(shared_memory, SHD_SPECTRE_LAB_SHARED_MEMORY_SIZE);
-            if current_offset > 4
+            if (current_offset > 3)
             {
                 for (int i = 0; i < 2000; i++) {
                     call_kernel_part2(shared_memory, 0);
